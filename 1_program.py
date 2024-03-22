cbd52d2e4a9e53514a952450ecba0c7e82fc3492
polina_storozh <58592174+voird@users.noreply.github.com>
2024-03-22 11:08:12 +0300

    Update 1_program.py

--- 1_program.py
+++ 1_program.py
@@ -3,3 +3,18 @@ import re
 def validate_phone_number(phone_number):
     pattern = r'^(\+?7|8)?[\s-]?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$'
     return bool(re.match(pattern, phone_number))
+
+def get_phone_number(phone_number):
+
+    if validate_phone_number(phone_number):
+        return phone_number
+    else:
+        raise ValueError("Некорректный телефонный номер")
+
+phone = input()
+
+try:
+    validated_phone = get_phone_number(phone)
+    print("Введенный номер:", validated_phone)
+except ValueError as e:
+    print(e)
